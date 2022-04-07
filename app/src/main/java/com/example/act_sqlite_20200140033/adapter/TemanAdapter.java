a7aef2fa804af0f86638e1cd0c658e17f7aa94a4
Reyfadh07 <raihan.farmasi@gmail.com>
2022-04-08 00:24:35 +0700

    Tugas activity SqLite_20200140033

---
+++ app/src/main/java/com/example/act_sqlite_20200140033/adapter/TemanAdapter.java
@@ -0,0 +1,66 @@
+package com.example.act_sqlite_20200140033.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.graphics.Color;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
+
+import com.example.act_sqlite_20200140033.R;
+import com.example.act_sqlite_20200140033.database.Teman;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false );
+        return new TemanViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+        String nm,tlp;
+
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+        holder.namaTxt.setTextColor(Color.BLUE);
+        holder.namaTxt.setTextSize(20);
+        holder.namaTxt.setText(nm);
+        holder.telponTxt.setText(tlp);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt,telponTxt;
+
+        public TemanViewHolder(View view) {
+            super(view);
+            cardku = (CardView) view.findViewById(R.id.kartuku);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+}
